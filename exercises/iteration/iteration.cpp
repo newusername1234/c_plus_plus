2ca2a129096c3ca2147bfb5800eca7b79513dd4c
Wes <wesleyayer92@gmail.com>
2020-06-04 09:50:27 -0400

    iteration

--- exercises/iteration/iteration.cpp
+++ exercises/iteration/iteration.cpp
@@ -1,10 +1,22 @@
 #include "../../std_lib_facilities.h"
 
+int square(int x)
+{
+    return x * x;
+}
+
+void write_sorry()
+{
+    cout << "sorry\n";
+}
+
 int main()
 {
-    int i = 97;
-    while (i<=122) {
-        cout << char(i) << '\t' << i << '\n';
-        ++i;
+    // for (int i = 97; i <= 122; ++i) {
+    //     cout << char(i) << '\t' << i << '\n';
+    // }
+    write_sorry();
+    for (int i = 0; i <= 100; ++i) {
+        cout << i << '\t' << square(i) << '\n';
     }
 }
